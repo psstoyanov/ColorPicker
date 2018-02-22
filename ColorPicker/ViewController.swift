4ed8d3b0bd5021f574d48a3ff2905e746b16e02e
PakoSt <kokotekilata@gmail.com>
2018-02-22 11:06:21 +0000

    L1_E14: Recreate ColorPicker initial state for the lesson.
    Instead of using the provided starting project, recreate the project locally and add
    the required views on the fresh project.
    This will avoid issues with the project setup, like migration to new versions of
    the libraries of Swift.

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISwitch!
+    @IBOutlet weak var greenControl: UISwitch!
+    @IBOutlet weak var blueControl: UISwitch!
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set colorView on launch
+        changeColorComponent()
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent() {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redControl == nil {
+            return
+        }
+        
+        let r: CGFloat = self.redControl.isOn ? 1 : 0
+        let g: CGFloat = self.greenControl.isOn ? 1 : 0
+        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
