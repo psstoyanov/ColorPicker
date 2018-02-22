3b3bf6ff762de96c241ebebd7c868334afcfe72d
PakoSt <kokotekilata@gmail.com>
2018-02-22 11:46:00 +0000

    L1_E23: Pass the slider values to the view
    The switches had only On/Off state. Whereas the sliders are passing
    the color value instead as CGFloat.

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -35,9 +35,9 @@ class ViewController: UIViewController {
             return
         }
         
-        let r: CGFloat = self.redControl.isOn ? 1 : 0
-        let g: CGFloat = self.greenControl.isOn ? 1 : 0
-        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+        let r: CGFloat = CGFloat(self.redControl.value);
+        let g: CGFloat = CGFloat(self.greenControl.value);
+        let b: CGFloat = CGFloat(self.blueControl.value);
         
         colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
